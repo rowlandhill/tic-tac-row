9deb8ef56bc1a129d90c423671184dd0edc6ca5d
rowlandhill <hill.rowland@gmail.com>
2017-04-11 11:35:27 -0400

    Rails, front-end and sign-up
    Enables the Rails Server, front-end grunt server.
    Sign-up button fires, but has not been finished.
    Has some html but no javascript. HTML shows errors in DOM because JS has
    not yet been written.

---
+++ assets/scripts/auth/events.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const getFormFields = require(`../../../lib/get-form-fields`)
+
+const api = require('./api')
+const ui = require('./ui')
+
+const onSignUp = function (event) {
+  console.log('event fired')
+  const data = getFormFields(this)
+  event.preventDefault()
+  api.signUp(data)
+    .then(ui.signUpSuccess)
+    .catch(ui.signUpFailure)
+}
+
+const addHandlers = () => {
+  $('#sign-up').on('submit', onSignUp)
+}
+
+module.exports = {
+  addHandlers
+}
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -10,5 +10,11 @@ $(() => {
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 
+const authEvents = require('./auth/events.js')
+
 // use require without a reference to ensure a file is bundled
 require('./example')
+
+$(() => {
+  authEvents.addHandlers()
+})
